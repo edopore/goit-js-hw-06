e6924d343c828fceab52b930613ac6b362f10729
Eduardo José Maya Rodriguez <36246464+edopore@users.noreply.github.com>
2023-12-02 09:32:53 -0500

    UPDATE se revierte cambio sobre tarea 10

--- js/task-10.js
+++ js/task-10.js
@@ -34,3 +34,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let width = 30;
+  for (let i = 0; i < amount; i++) {
+    let newBox = document.createElement("div");
+    newBox.setAttribute("id", `box`);
+    newBox.style.width = `${width}px`;
+    newBox.style.height = `${width}px`;
+    newBox.style.background = `${getRandomHexColor()}`;
+    divBoxes.append(newBox);
+    width += 10;
+  }
+}
+
+function destroyBoxes() {
+  const parent = document.getElementById("boxes");
+  while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
+}
+
+const controlButtons = document.querySelectorAll("button");
+const inputValue = document.querySelector("input");
+const divBoxes = document.querySelector("#boxes");
+
+const action = (event) => {
+  switch (event.target.attributes[1].name) {
+    case "data-create":
+      createBoxes(inputValue.value);
+      break;
+    case "data-destroy":
+      destroyBoxes();
+      break;
+    default:
+      console.log("Valor inválido");
+  }
+};
+controlButtons[0].addEventListener("click", action);
+controlButtons[1].addEventListener("click", action);
